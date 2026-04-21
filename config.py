685b82d3ac1104c27f806d9bd650c50496030b00
Yann Leão <yann.kjleao@ufrpe.br>
2026-04-21 15:21:17 -0300

    style: add theme.json for interface theming

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
+from pathlib import Path
+
 from algorithms.caesar import encrypt as caesar_encrypt, decrypt as caesar_decrypt, generate_key as caesar_generate
+from algorithms.hill import encrypt as hill_encrypt, decrypt as hill_decrypt, generate_key as hill_generate
 from algorithms.mono_alphabetic import (encrypt as mono_alphabetic_encrypt, decrypt as mono_alphabetic_decrypt,
                                         generate_key as mono_alphabetic_generate)
 from algorithms.playfair import (encrypt as playfair_encrypt, decrypt as playfair_decrypt,
                                  generate_key as playfair_generate)
-from algorithms.hill import encrypt as hill_encrypt, decrypt as hill_decrypt, generate_key as hill_generate
+
+BASE_DIR = Path(__file__).resolve().parent
 
 ALGORITHMS = {
     "Caesar": {
@@ -27,3 +31,9 @@ ALGORITHMS = {
         "generate_key": hill_generate
     },
 }
+
+# UI/UX
+THEME_PATH = BASE_DIR / "ui" / "theme.json"
+BACKGROUND_COLOR = "#0E130E"
+BARS_COLOR = "#00E68A"
+TEXT_COLOR = "#9EFFC8"
